c7ebbdd2740ab2159cbb7c93ce7af9f8baf1fc39
Will Zhang <32278614+WillCZhang@users.noreply.github.com>
2026-08-24 09:55:53 -0700

    chore: log metrics SQL query

--- server/api/stats/metrics.get.ts
+++ server/api/stats/metrics.get.ts
@@ -32,5 +32,6 @@ function query2sql(query: z.infer<typeof MetricsQuerySchema>, event: H3Event) {
 export default eventHandler(async (event) => {
   const query = await getValidatedQuery(event, MetricsQuerySchema.parse)
   const sql = query2sql(query, event)
+  console.log('Metrics SQL:', sql.explain())
   return useWAE(event, sql)
 })
