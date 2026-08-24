1e690c788659026434cd6dc7d5e9d89fce77205b
Will Zhang <32278614+WillCZhang@users.noreply.github.com>
2026-08-24 10:33:50 -0700

    fix credential config

--- server/utils/cloudflare.ts
+++ server/utils/cloudflare.ts
@@ -2,15 +2,20 @@ import type { H3Event } from 'h3'
 import type { Compilable } from 'kysely'
 
 export function useWAE(event: H3Event, query: Compilable) {
-  const { cfAccountId, cfApiToken } = useRuntimeConfig(event)
-  console.log('useWAE', { cfAccountId, cfApiToken })
+  // i don't like that i need to get entire config here...
+  // but seems like this global context being passed around anyways...
+  // so i guess this is how node people would do it...?
+  const config = useRuntimeConfig(event)
+
+  const cfAccountId = event.context.cloudflare?.env?.NUXT_CF_ACCOUNT_ID || config.cfAccountId
+  const cfApiToken = event.context.cloudflare?.env?.NUXT_CF_API_TOKEN || config.cfApiToken
   if (!cfAccountId || !cfApiToken)
     return { data: [] }
 
   const compiledQuery = compileAnalyticsQuery(query)
 
-  // if (import.meta.dev)
-  console.info('useWAE', compiledQuery)
+  if (import.meta.dev)
+    console.info('useWAE', compiledQuery)
 
   return $fetch(`https://api.cloudflare.com/client/v4/accounts/${cfAccountId}/analytics_engine/sql`, {
     method: 'POST',
